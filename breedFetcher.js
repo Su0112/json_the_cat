b5c7b6ddbb11a1173656fc0a6ab9ece579fc24b4
SuShruthi <shruthacm@gmail.com>
2023-04-22 09:11:53 -0700

    add code to request to fetch data from the API endpoint for the Siberian cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
 const request = require('request');
+
+const breedName = 'siberian';
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.log('Status:', response.statusCode);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data);
+  }
+});
