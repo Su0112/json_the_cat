9ee10158ce9f8e493a51cb38d8ccff25726a6158
SuShruthi <shruthacm@gmail.com>
2023-04-22 09:18:42 -0700

    add code to output an appropriate message if the requested breed is not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,30 @@
 const request = require('request');
 
+// Get the breed name from the command line arguments
 const breedName = process.argv[2];
+
+// Build the API endpoint URL with the breed name as the query parameter
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+// Make a request to the API endpoint
 request(url, (error, response, body) => {
   if (error) {
-    console.log('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.log('Status:', response.statusCode);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
-    } else {
-      console.log(data[0].description);
-    }
+    console.error(`Error making request: ${error}`);
+    return;
   }
+
+  // Parse the response body to get an object representing the data
+  const data = JSON.parse(body);
+
+  // Check if the data array is empty
+  if (data.length === 0) {
+    console.log(`Breed '${breedName}' not found`);
+    return;
+  }
+
+  // Get the first entry in the data array
+  const breed = data[0];
+
+  // Print out the description for the user
+  console.log(breed.description);
 });
