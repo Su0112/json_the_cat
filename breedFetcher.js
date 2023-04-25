3fdb5ab2bfd5539056e401f7016ea6c7d3b77a10
SuShruthi <shruthacm@gmail.com>
2023-04-25 16:42:53 -0700

    refactored the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,32 @@
 const request = require('request');
 
-// Get the breed name from the command line arguments
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  // Build the API endpoint URL with the breed name as the query parameter
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// Build the API endpoint URL with the breed name as the query parameter
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  // Make a request to the API endpoint
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-// Make a request to the API endpoint
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error making request: ${error}`);
-    return;
-  }
+    // Parse the response body to get an object representing the data
+    const data = JSON.parse(body);
 
-  // Parse the response body to get an object representing the data
-  const data = JSON.parse(body);
+    // Check if the data array is empty
+    if (data.length === 0) {
+      callback(`Breed '${breedName}' not found`, null);
+      return;
+    }
 
-  // Check if the data array is empty
-  if (data.length === 0) {
-    console.log(`Breed '${breedName}' not found`);
-    return;
-  }
+    // Get the first entry in the data array
+    const breed = data[0];
 
-  // Get the first entry in the data array
-  const breed = data[0];
+    //call the callbacks
+    callback(null, breed.description);
 
-  // Print out the description for the user
-  console.log(breed.description);
-});
+  });
+
+};
+module.exports = { fetchBreedDescription };
